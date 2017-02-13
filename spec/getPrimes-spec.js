6b7e0abbd097b18431c97e868cf3befb7d493799
Solomon Kingsley <johnkingzy@gmail.com>
2017-02-13 12:13:13 +0100

    I just wrote some test case to test for negative input

--- spec/getPrimes-spec.js
+++ spec/getPrimes-spec.js
@@ -3,32 +3,36 @@
 var app = require("../app/library.js");
 describe("Cases for invalid input", function() {
 	describe("Cases for empty input", function() {
-		it("should return `No input Please enter a number` for ()", function() {
-			expect(app.getPrimes()).toBe('No input Please enter a number');
+		it("should return `Invalid input! only positive numbers are allowed` for ()", function() {
+			expect(app.getPrimes()).toBe('Invalid input! only positive numbers are allowed');
 		});
 	});
 
 	describe("Cases for Strings as input", function() {
-		it("should return `Invalid input only numbers are allowed` for 'hello'", function() {
-			expect(app.getPrimes('hello')).toBe('Invalid input only numbers are allowed');
+		it("should return `Invalid input! only positive numbers are allowed` for 'hello'", function() {
+			expect(app.getPrimes('hello')).toBe('Invalid input! only positive numbers are allowed');
 		});
 
-		it("should return `Invalid input only numbers are allowed` for 'world'", function() {
-			expect(app.getPrimes('world')).toBe('Invalid input only numbers are allowed');
+		it("should return `Invalid input! only positive numbers are allowed` for 'world'", function() {
+			expect(app.getPrimes('world')).toBe('Invalid input! only positive numbers are allowed');
 		});
 
-		it("should return `Invalid input only numbers are allowed` for 'mytest'", function() {
-			expect(app.getPrimes('mytest')).toBe('Invalid input only numbers are allowed');
+		it("should return `Invalid input! only positive numbers are allowed` for 'mytest'", function() {
+			expect(app.getPrimes('mytest')).toBe('Invalid input! only positive numbers are allowed');
 		});
 	});
 
 	describe("Cases for Arrays as Input", function() {
-		it("should return `Invalid input only numbers are allowed` for []", function() {
-			expect(app.getPrimes([])).toBe('Invalid input only numbers are allowed');
+		it("should return `Invalid input! only positive numbers are allowed` for []", function() {
+			expect(app.getPrimes([])).toBe('Invalid input! only positive numbers are allowed');
 		});
 
-		it("should return `Invalid input only numbers are allowed` for [1, 2, 3, 4]", function() {
-			expect(app.getPrimes([1, 2, 3, 4])).toBe('Invalid input only numbers are allowed');
+		it("should return `Invalid input! only positive numbers are allowed` for [1, 2, 3, 4]", function() {
+			expect(app.getPrimes([1, 2, 3, 4])).toBe('Invalid input! only positive numbers are allowed');
+		});
+
+		it("should return `Invalid input! only positive numbers are allowed` for -10", function() {
+			expect(app.getPrimes(-10)).toBe('Invalid input! only positive numbers are allowed');
 		});
 	});
 	describe("Cases For Numbers as input", function(){
@@ -38,6 +42,14 @@ describe("Cases for invalid input", function() {
 
 		it("should return [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] for 30", function() {
 			expect(app.getPrimes(30)).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
+		});
+
+		it("should return [2, 3, 5, 7, 11, 13, 17, 19, 23] for 23", function() {
+			expect(app.getPrimes(23)).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23]);
+		});
+
+		it("should return [2, 3, 5, 7] for 7", function() {
+			expect(app.getPrimes(7)).toEqual([2, 3, 5, 7]);
 		});
 
 		it("should return [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47] for 50", function() {
