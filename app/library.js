e68bb80a6b8628394fbf658a13cd999f0691f5c1
Solomon Kingsley <johnkingzy@gmail.com>
2017-02-13 01:15:25 +0100

    I just wrote my mathematical formular to test if a number is prime

---
+++ app/library.js
@@ -0,0 +1,16 @@
+//My function to pass all my test case
+//My formular to check for prime numbers 
+const myFormula = {
+	isPrime: function(n) {
+		const solveFirst = Math.floor(n / 2);
+		const getFirst = 2 * solveFirst;
+		const = n - getFirst;
+		return firstResult;
+	},
+	checkPrime: function(n) {
+		const solveFirst = Math.floor(n / 2);
+		const getFirst = 3 * solveFirst;
+		const firstResult = n - getFirst;
+		return firstResult;
+	}
+};
