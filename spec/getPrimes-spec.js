01d1414099dd078be50f2f036e4052283c916f9b
Solomon Kingsley <johnkingzy@gmail.com>
2017-02-13 01:06:25 +0100

    i just add the test case for numbers

--- spec/getPrimes-spec.js
+++ spec/getPrimes-spec.js
@@ -12,11 +12,50 @@ describe("Cases for invalid input", function() {
 		it("should return `Invalid input only numbers are allowed` for 'hello'", function() {
 			expect(app.getPrimes('hello')).toBe('Invalid input only numbers are allowed');
 		});
+
+		it("should return `Invalid input only numbers are allowed` for 'world'", function() {
+			expect(app.getPrimes('world')).toBe('Invalid input only numbers are allowed');
+		});
+
+		it("should return `Invalid input only numbers are allowed` for 'mytest'", function() {
+			expect(app.getPrimes('mytest')).toBe('Invalid input only numbers are allowed');
+		});
 	});
 
 	describe("Cases for Arrays as Input", function() {
-		it("should return `Invalid input only numbers are allowed` for []", function () {
+		it("should return `Invalid input only numbers are allowed` for []", function() {
 			expect(app.getPrimes([])).toBe('Invalid input only numbers are allowed');
 		});
+
+		it("should return `Invalid input only numbers are allowed` for [1, 2, 3, 4]", function() {
+			expect(app.getPrimes([1, 2, 3, 4])).toBe('Invalid input only numbers are allowed');
+		});
 	});
+	describe("Cases For Numbers as input", function(){
+		it("should return [2, 3, 5, 7, 11, 13, 17, 19] for 20", function() {
+			expect(app.getPrimes(20))toBe([2, 3, 5, 7, 11, 13, 17, 19]);
+		});
+
+		it("should return [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] for 30", function() {
+			expect(app.getPrimes(30))toBe([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
+		});
+
+		it("should return [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47] for 50", function() {
+			expect(app.getPrimes(50))toBe([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]);
+		});
+
+		it("should return [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47] for 40", function() {
+			expect(app.getPrimes(40))toBe([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]);
+		});
+
+		it("should return [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67] for 70", function() {
+			expect(app.getPrimes(70))toBe([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67]);
+		});
+
+		it("should return [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97] for 100", function() {
+			expect(app.getPrimes(70))toBe([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]);
+		});
+
+	});
+
 });
