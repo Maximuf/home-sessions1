aa3ee926d221fdfdae195441f7b7d3f4a9b21e27
Solomon Kingsley <johnkingzy@gmail.com>
2017-02-13 12:12:07 +0100

    I just validate the input for negative numbers

--- app/library.js
+++ app/library.js
@@ -3,39 +3,38 @@
 const myFormula = {
 	//check if a number is prime
 	checkPrime: function(n) {
-		return !(Array(n + 1).join(1).match(/^1?$|^(11+?)\1+$/)); //Using Regexp to match numbers
+		return !(Array(n + 1).join(1).match(/^1?$|^(11+?)\1+$/)); //Using Regexp
 	}
 };
 const check = {
 	getPrimes: function(n) {
-		//check if n does not exist
-		if(!(n)) {
-			const msg = "No input Please enter a number";
-			return msg;
-		} 
-		//check if n is an array
-		else if(Array.isArray(n)) {
-			const msg = "Invalid input only numbers are allowed";
-			return msg;
-		} 
-		//check if n is not a number
-		else if(isNaN(n)) {
-			const msg = "Invalid input only numbers are allowed";
+		if(isNaN(n) || parseInt(n) < 0) {
+			const msg = "Invalid input! only positive numbers are allowed";
 			return msg;
 		}
+		//check if n is an array
+		if(Array.isArray(n)) {
+			const msg = "Invalid input! only positive numbers are allowed";
+			return msg;
+		} 
 		var result = [];
 		//Generating an array of numbers from 0 to n
-		const list = Array.from(new Array(n), (val,index)=>index);
+		const list = Array.from(new Array(n), (val,index)=>index + 1);
 
 		//filter every element that is a prime number
 		const primeNumbers = list.filter(function (lists) {
+		  if(lists === 2) {
+		    result.push(2);
+		  }
+		  if(lists === 3) {
+		    result.push(3);
+		  }
 			if(lists > 3) {
 				return myFormula.checkPrime(lists) === true;
 			}
 		});
 		
-		result.push(2);
-		result.push(3);
+		
 		const finalResult = result.concat(primeNumbers);
 		return finalResult;
 	}
